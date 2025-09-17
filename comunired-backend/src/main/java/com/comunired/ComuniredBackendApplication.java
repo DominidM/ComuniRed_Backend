a7c3096f0227704c7fb06f1c9399f36caf814d89
Dominid Muñoz Eslava <dominidzero@gmail.com>
2025-09-17 01:26:12 -0500

    [DME] 17092025 - inicio

---
+++ comunired-backend/src/main/java/com/comunired/ComuniredBackendApplication.java
@@ -0,0 +1,13 @@
+package com.comunired;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComuniredBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComuniredBackendApplication.class, args);
+	}
+
+}
---
+++ comunired-backend/src/test/java/com/comunired/ComuniredBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comunired;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComuniredBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
