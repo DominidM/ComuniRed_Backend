36cb88cf6d2c822443f9eb77c373442f3b804138
Dominid Muñoz Eslava <dominidzero@gmail.com>
2025-09-18 20:42:05 -0500

    [DME] 18092025 - inicio DDD

--- comunired-backend/src/main/java/com/comunired/roles/domain/entity/Rol.java
+++ comunired-backend/src/main/java/com/comunired/roles/domain/entity/Rol.java
@@ -1,6 +1,11 @@
 package com.comunired.roles.domain.entity;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "roles")
 public class Rol {
+    @Id
     private String id;
     private String nombre;
     private String descripcion;
